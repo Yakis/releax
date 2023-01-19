b2863466a4fc33b62eaa62491b186236628b2211
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2023-01-19 21:52:19 +0000

    Initial Commit

---
+++ ReleaX/ReleaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReleaXApp.swift
+//  ReleaX
+//
+//  Created by Mugurel Moscaliuc on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReleaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
